94c1f9cff389d29d476f954329d0ce061701b276
Alejandro Pérez <133078626+Alejandroperezitsur@users.noreply.github.com>
2025-10-13 23:56:07 -0600

    Se actualizó el estado de la UI

--- app/src/main/java/com/example/inventory/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/example/inventory/ui/AppViewModelProvider.kt
@@ -4,8 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ * *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,43 +17,41 @@ package com.example.inventory.ui
 
 import android.app.Application
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
-import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.inventory.InventoryApplication
 import com.example.inventory.ui.home.HomeViewModel
-import com.example.inventory.ui.item.ItemDetailsViewModel
-import com.example.inventory.ui.item.ItemEditViewModel
-import com.example.inventory.ui.item.ItemEntryViewModel
 
 /**
  * Provides Factory to create instance of ViewModel for the entire Inventory app
  */
 object AppViewModelProvider {
     val Factory = viewModelFactory {
-        // Initializer for ItemEditViewModel
-        initializer {
-            ItemEditViewModel(
-                this.createSavedStateHandle()
-            )
-        }
-        // Initializer for ItemEntryViewModel
-        initializer {
-            ItemEntryViewModel(inventoryApplication().container.itemsRepository)
-        }
-
-        // Initializer for ItemDetailsViewModel
-        initializer {
-            ItemDetailsViewModel(
-                this.createSavedStateHandle()
-            )
-        }
-
         // Initializer for HomeViewModel
         initializer {
-            HomeViewModel()
+            HomeViewModel(inventoryApplication().container.itemsRepository)
         }
+        // Initializer for ItemEntryViewModel
+        // initializer {
+        //     ItemEntryViewModel(inventoryApplication().container.itemsRepository)
+        // }
+
+        // Initializer for ItemDetailsViewModel
+        // initializer {
+        //     ItemDetailsViewModel(
+        //         this.createSavedStateHandle(),
+        //         inventoryApplication().container.itemsRepository
+        //     )
+        // }
+
+        // Initializer for ItemEditViewModel
+        // initializer {
+        //     ItemEditViewModel(
+        //         this.createSavedStateHandle(),
+        //         inventoryApplication().container.itemsRepository
+        //     )
+        // }
     }
 }
 

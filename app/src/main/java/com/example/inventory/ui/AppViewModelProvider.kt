8a80a248093170d632a31206679ae7f77ce9a22f
Alejandro Pérez <133078626+Alejandroperezitsur@users.noreply.github.com>
2025-10-14 12:30:53 -0600

    Muestra los detalles del elemento

--- app/src/main/java/com/example/inventory/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/example/inventory/ui/AppViewModelProvider.kt
@@ -4,7 +4,8 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,11 +18,13 @@ package com.example.inventory.ui
 
 import android.app.Application
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.inventory.InventoryApplication
 import com.example.inventory.ui.home.HomeViewModel
+import com.example.inventory.ui.item.ItemDetailsViewModel
 
 /**
  * Provides Factory to create instance of ViewModel for the entire Inventory app
@@ -38,12 +41,12 @@ object AppViewModelProvider {
         // }
 
         // Initializer for ItemDetailsViewModel
-        // initializer {
-        //     ItemDetailsViewModel(
-        //         this.createSavedStateHandle(),
-        //         inventoryApplication().container.itemsRepository
-        //     )
-        // }
+        initializer {
+            ItemDetailsViewModel(
+                this.createSavedStateHandle(),
+                inventoryApplication().container.itemsRepository
+            )
+        }
 
         // Initializer for ItemEditViewModel
         // initializer {
